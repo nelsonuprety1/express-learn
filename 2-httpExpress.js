7a8a6c5ca473d17a3db349e2b30e20124284a64e
Nelson Uprety <nelsonuprety3@gmail.com>
2022-03-24 12:04:14 +0000

    Basic example of express server

---
+++ 2-httpExpress.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Home Page');
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).send('About Page');
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>Resource not found</h1>');
+});
+
+app.listen(3000, () => {
+  console.log(`Server started on port 3000`);
+});
+
+/*
+app.get
+app.post
+app.put
+app.delete
+app.all
+app.use 
+app.listen
+*/
