08e20b3dcce3d812ab1af5719de4fb6816bb882a
Nelson Uprety <nelsonuprety3@gmail.com>
2022-03-24 11:36:35 +0000

    adding more logic and editing

--- 1-http.js
+++ 1-http.js
@@ -1,8 +1,39 @@
 const http = require('http');
+const { readFileSync } = require('fs');
+
+// get all files
+const homePage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer((req, res) => {
-  console.log('User hit the server');
-  res.end('Welcome to our server');
+  const url = req.url;
+  console.log(url);
+  // homepage
+  if (url === '/') {
+    /**
+     * The response.writeHead() (Added in v1..0) property is an inbuilt property of the ‘http’ module
+     *  which sends a response header to the request. The status code is a 3-digit HTTP status code,
+     * like 404. The last argument, headers, are the response headers.
+     * Optionally one can give a human-readable statusMessage as the second argument.
+     */
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write(homePage);
+    res.end();
+  }
+  // about page
+  else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>About page</h1>');
+    res.end();
+
+    // resource not found
+  } else {
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.write('<h1>Content Not Found</h1>');
+    res.end();
+  }
 });
 
 server.listen(3000, () => {
