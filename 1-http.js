af69332f50204571caedbb0a7c19ccccf642023b
Nelson Uprety <nelsonuprety3@gmail.com>
2022-03-24 11:41:55 +0000

    Serving webpage without express

--- 1-http.js
+++ 1-http.js
@@ -9,7 +9,6 @@ const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  console.log(url);
   // homepage
   if (url === '/') {
     /**
@@ -27,9 +26,28 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'content-type': 'text/html' });
     res.write('<h1>About page</h1>');
     res.end();
+  }
+  // styles
+  else if (url === '/styles.css') {
+    res.writeHead(200, { 'content-type': 'text/css' });
+    res.write(homeStyles);
+    res.end();
+  }
 
-    // resource not found
-  } else {
+  // logo
+  else if (url === '/logo.svg') {
+    res.writeHead(200, { 'content-type': 'image/svg+xml' });
+    res.write(homeImage);
+    res.end();
+  }
+  // logic
+  else if (url === '/browser-app.js') {
+    res.writeHead(200, { 'content-type': 'text/javascript' });
+    res.write(homeLogic);
+    res.end();
+  }
+  // resource not found
+  else {
     res.writeHead(404, { 'content-type': 'text/html' });
     res.write('<h1>Content Not Found</h1>');
     res.end();
